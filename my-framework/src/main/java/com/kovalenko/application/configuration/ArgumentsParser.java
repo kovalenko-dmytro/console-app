066cfc3916c31d2f9489db2e481533bc6b3b717a
kovalenko-dmytro <kovalenko.dm80@gmail.com>
2020-04-28 08:53:38 +0300

    add argument parser unit tests

--- my-framework/src/main/java/com/kovalenko/application/configuration/ArgumentsParser.java
+++ my-framework/src/main/java/com/kovalenko/application/configuration/ArgumentsParser.java
@@ -1,9 +1,7 @@
 package com.kovalenko.application.configuration;
 
-import com.kovalenko.application.exception.ApplicationException;
-
 import java.util.Map;
 
 public interface ArgumentsParser {
-    Map<String, String> parse(String... args) throws ApplicationException;
+    Map<String, String> parse(String... args);
 }
---
+++ my-framework/src/test/java/com/kovalenko/application/configuration/console/ConsoleArgumentsParserTest.java
@@ -0,0 +1,33 @@
+package com.kovalenko.application.configuration.console;
+
+import com.kovalenko.application.configuration.ArgumentsParser;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ConsoleArgumentsParserTest {
+
+    private String[] args;
+
+    @BeforeEach
+    void setUp() {
+        args = new String[]{
+            "-config=path/to/config",
+            "-script=path/to/script",
+            "failArg=fail"
+        };
+    }
+
+    @Test
+    void parse() {
+        ArgumentsParser parser = new ConsoleArgumentsParser();
+        Map<String, String> actual = parser.parse(args);
+
+        assertNotNull(actual);
+        assertTrue(actual.size() > 0);
+        assertEquals(2, actual.size());
+    }
+}
