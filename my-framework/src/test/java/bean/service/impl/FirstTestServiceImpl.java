2c6921adc9adece62394f1d4a4fe15011af32396
kovalenko-dmytro <kovalenko.dm80@gmail.com>
2020-04-30 01:10:08 +0300

    add app runner unit tests

--- my-framework/src/test/java/bean/service/impl/FirstTestServiceImpl.java
+++ my-framework/src/test/java/bean/service/impl/FirstTestServiceImpl.java
@@ -7,6 +7,6 @@ import com.kovalenko.ioc.bean.factory.stereotype.Service;
 public class FirstTestServiceImpl implements FirstTestService {
     @Override
     public void test(String param1, String param2, String param3) {
-        System.out.println();
+        System.out.println(String.join(" ", param1, param2, param3));
     }
 }
---
+++ my-framework/src/test/java/com/kovalenko/application/runner/ApplicationRunnerTest.java
@@ -0,0 +1,37 @@
+package com.kovalenko.application.runner;
+
+import com.kovalenko.application.BaseTest;
+import com.kovalenko.application.exception.ApplicationException;
+import com.kovalenko.application.runner.constant.RunnerConstant;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashMap;
+
+class ApplicationRunnerTest extends BaseTest {
+
+    private static final Path FILE_PATH = Paths.get("src", "test", "resources", "testFile.txt");
+    private static final String TEST_COMMAND = "test -param1 value1 -param2 value2 -param3 ".concat(FILE_PATH.toString());
+    private static final String EXIT = RunnerConstant.EXIT_COMMAND_NAME.getValue();
+
+    private InputStream stdin = System.in;
+    private PrintStream stdout = System.out;
+
+    @Test
+    void runApplication() throws ApplicationException {
+        try {
+            System.setIn(new ByteArrayInputStream(TEST_COMMAND.concat(System.lineSeparator()).concat(EXIT).getBytes()));
+            ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+            System.setOut(new PrintStream(outContent));
+            ApplicationRunner.getInstance().run(new HashMap<>());
+        } finally {
+            System.setIn(stdin);
+            System.setOut(stdout);
+        }
+    }
+}
