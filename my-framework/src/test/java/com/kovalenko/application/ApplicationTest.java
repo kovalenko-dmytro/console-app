559531f693e2265f79b2a49f4be9891646144ec4
kovalenko-dmytro <kovalenko.dm80@gmail.com>
2020-04-30 10:02:55 +0300

    add application unit tests

---
+++ my-framework/src/test/java/com/kovalenko/application/ApplicationTest.java
@@ -0,0 +1,32 @@
+package com.kovalenko.application;
+
+import bean.NotAnnotatedLaunchClass;
+import com.kovalenko.application.message.MessageSource;
+import com.kovalenko.application.message.impl.SystemMessageSource;
+import com.kovalenko.ioc.annotation.ConsoleApplication;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class ApplicationTest extends BaseTest {
+
+    private PrintStream stdout = System.out;
+    private MessageSource messageSource = SystemMessageSource.getInstance();
+
+    @Test
+    void whenLaunchClassIsNotAnnotatedShouldThrown() {
+        try {
+            ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+            System.setOut(new PrintStream(outContent));
+
+            Application.launch(NotAnnotatedLaunchClass.class);
+            String actual = outContent.toString().replace(System.lineSeparator(), "");
+            assertEquals(messageSource.getMessage("error.annotation.wasnt.defined", ConsoleApplication.class), actual);
+        } finally {
+            System.setOut(stdout);
+        }
+    }
+}
